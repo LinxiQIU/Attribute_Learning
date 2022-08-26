05a77689c35e8d7d22e970b15047f6f47245823c
LinxiQiu <linuxqlx@gmail.com>
2022-08-27 00:36:50 +0200

    first commit

---
+++ data_attr.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 01:25:31 2022
+
+@author: linxi
+"""
+
+import pandas as pd
+import numpy as np
+import os
+from tqdm import tqdm
+from torch.utils.data import Dataset
+import torch
+
+class MotorAttribute(Dataset):
+    def __init__(self, root_dir, csv_file, split='train', npoints=2048, test_area='Validation'):
+        super().__init__()
+        self.root_dir = root_dir
+        self.npoints = npoints
+        self.data = pd.read_csv(csv_file)
+        motor_ls = sorted(os.listdir(root_dir))
+        if split == 'train':
+            motor_ids = [motor for motor in motor_ls if '{}'.format(test_area) not in motor]
+        else:
+            motor_ids = [motor for motor in motor_ls if '{}'.format(test_area) in motor]
+        self.all_points = []
+        self.all_attr = []
+        self.all_type = []
+        self.all_bolt_num = []
+        num_points_eachmotor= []
+        for idx in tqdm(motor_ids, total=len(motor_ids)):
+            point_set = np.load(os.path.join(root_dir, idx))[:, 0:3]
+            self.all_points.append(point_set)
+            num_points_eachmotor.append(point_set.size)
+            n = idx.split('_')
+            attr_data = self.data[self.data['Nr.'].str.contains(n[1] + '_' + n[2])]
+            attr = np.array(attr_data.iloc[:, 3:]).astype('float')
+            ty = attr_data.iloc[:, 1].tolist()
+            num = attr_data.iloc[:, 2].tolist()
+            self.all_attr.append(attr)
+            self.all_type.append(ty)
+            self.all_bolt_num.append(num)
+        
+        
+        sample_prob = num_points_eachmotor / np.sum(num_points_eachmotor)
+        num_inter = np.sum(num_points_eachmotor) / self.npoints
+        self.motor_idxs = []
+        for idx in range(len(num_points_eachmotor)):
+            sample_times_onemotor = int(round(sample_prob[idx] * num_inter))
+            motor_idx_onemotor = [idx] * sample_times_onemotor
+            self.motor_idxs.extend(motor_idx_onemotor)
+        
+    def __len__(self):
+        return len(self.motor_idxs)
+    
+    def __getitem__(self, index):
+        point_set = self.all_points[self.motor_idxs[index]]
+        
+        types = self.all_type[self.motor_idxs[index]]
+        
+        attribute = self.all_attr[self.motor_idxs[index]]
+        
+        cover_bolt_num = self.all_bolt_num[self.motor_idxs[index]]
+        n_points = point_set.shape[0]
+        chosed = np.random.choice(n_points, self.npoints, replace=True)
+        chosed_pc = point_set[chosed, :]
+        sample = {'point': chosed_pc, 'attribute': torch.Tensor(attribute), 
+                  'type': torch.Tensor(types), 'num': torch.Tensor(cover_bolt_num)}
+        return sample
+
+
+if __name__ == '__main__':
+    train_data = MotorAttribute(root_dir='E:\\dataset1000', csv_file='E:\\data\\motor_attr.csv', 
+                          split='train')
+    train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=2, shuffle=True, drop_last=True)
+    for data in train_dataloader:
+        print(data['attribute'], data['type'], data['num'])
+        
---
+++ models.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 18 01:31:11 2022
+
+@author: linux
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from pointnet_utils import PointNetSetAbstraction
+
+
+def knn(x, k):
+    """
+    Input:
+        points: input points data, [B, C, N]
+    Return:
+        idx: sample index data, [B, N, K]
+    """
+    inner = -2*torch.matmul(x.transpose(2, 1), x)
+    xx = torch.sum(x**2, dim=1, keepdim=True)
+    pairwise_distance = -xx - inner - xx.transpose(2, 1)
+    idx = pairwise_distance.topk(k=k, dim=-1)[1]   #(batch_size, num_points, k)
+    return idx
+
+
+def get_graph_feature(x, k=20, idx=None):
+    batch_size = x.size(0)
+    num_points = x.size(1)
+    num_dims = x.size(2)
+    x = x.reshape(batch_size, -1, num_points)
+    idx = knn(x, k)
+    device = torch.device('cuda')    
+    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
+    idx += idx_base
+    x = x.transpose(2, 1).contiguous()
+    feature = x.view(batch_size*num_points, -1)[idx, :]
+    feature = feature.view(batch_size, num_points, k, num_dims)
+    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
+    feature = torch.cat((feature-x, x), dim=3).permute(0, 3, 1, 2).contiguous()
+    return feature
+    
+
+def index_points_neighbors(x, idx):
+    """
+    Input:
+        points: input points data, [B, N, C]
+        idx: sample index data, [B, N, K]
+    Return:
+        new_points:, indexed points data, [B, N, K, C]
+    """
+    batch_size = x.size(0)
+    num_points = x.size(1)
+    num_dims= x.size(2)
+
+    device=idx.device
+    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
+    idx=idx+idx_base
+    neighbors = x.view(batch_size*num_points, -1)[idx, :]
+    neighbors = neighbors.view(batch_size, num_points, -1, num_dims)
+
+    return neighbors
+
+
+
+def get_neighbors(x, k=20):
+    """
+    Input:
+        points: input points data, [B, C, N]
+    Return:
+        feature_points:, indexed points data, [B, 2*C, N, K]
+    """
+    batch_size = x.size(0)
+    num_dims= x.size(1)
+    num_points = x.size(2)
+    idx = knn(x, k)                                         # batch_size x num_points x 20
+    x = x.transpose(2, 1).contiguous()
+    neighbors = index_points_neighbors(x, idx)  
+    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1) 
+    feature = torch.cat((neighbors-x, x), dim=3).permute(0, 3, 1, 2).contiguous()
+
+    return feature
+
+    
+class DGCNN_net(nn.Module):
+    def __init__(self, output_channels=5):
+        super(DGCNN_net, self).__init__()
+        self.k = 32
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm1d(1024)
+        
+        self.conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=1, bias=False),
+                                   self.bn1, 
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv2 = nn.Sequential(nn.Conv2d(64*2, 64, kernel_size=1, bias=False),
+                                   self.bn2, 
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv3 = nn.Sequential(nn.Conv2d(64*2, 128, kernel_size=1, bias=False),
+                                   self.bn3, 
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv4 = nn.Sequential(nn.Conv2d(128*2, 256, kernel_size=1, bias=False),
+                                   self.bn4, 
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv5 = nn.Sequential(nn.Conv1d(512, 1024, kernel_size=1, bias=False),
+                                   self.bn5, 
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.linear1 = nn.Linear(1024*2, 512, bias=False)
+        self.bn6 = nn.BatchNorm1d(512)
+        self.dp1 = nn.Dropout(0.5)
+        self.linear2 = nn.Linear(512, 256)
+        self.bn7 = nn.BatchNorm1d(256)
+        self.dp2 = nn.Dropout(0.5)
+        self.linear3 = nn.Linear(256, output_channels)
+        self.fc1 = nn.Linear(1024*2, 512, bias=False)
+        self.bn8 = nn.BatchNorm1d(512)
+        self.drop1 = nn.Dropout(0.4)
+        self.fc2 = nn.Linear(512, 256)
+        self.bn9 = nn.BatchNorm1d(256)
+        self.drop2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(256, 5)
+        
+        self.fc4 = nn.Linear(1033, 512)
+        # self.bn10 = nn.BatchNorm1d(512)
+        self.drop3 = nn.Dropout(0.5)
+        self.fc5 = nn.Linear(512, 256)
+        self.bn11 = nn.BatchNorm1d(256)
+        self.drop4 = nn.Dropout(0.5)
+        self.fc6 = nn.Linear(256, 28)
+        
+
+       
+    def forward(self, x):
+        batch_size = x.size(0)
+        
+        x = get_neighbors(x, k=self.k)      # (batch_size, 6, num_points) -> (batch_size, 6*2, num_points, k)
+        x = self.conv1(x)                       # (batch_size, 6*2, num_points, k) -> (batch_size, 64, num_points, k)
+        x1 = x.max(dim=-1, keepdim=False)[0]    # (batch_size, 64, num_points, k) -> (batch_size, 64, num_points)
+
+        x = get_neighbors(x1, k=self.k)     # (batch_size, 64, num_points) -> (batch_size, 64*2, num_points, k)
+        x = self.conv2(x)                       # (batch_size, 64*2, num_points, k) -> (batch_size, 64, num_points, k)
+        x2 = x.max(dim=-1, keepdim=False)[0]    # (batch_size, 64, num_points, k) -> (batch_size, 64, num_points)
+
+        x = get_neighbors(x2, k=self.k)     # (batch_size, 64, num_points) -> (batch_size, 64*2, num_points, k)
+        x = self.conv3(x)                       # (batch_size, 64*2, num_points, k) -> (batch_size, 128, num_points, k)
+        x3 = x.max(dim=-1, keepdim=False)[0]    # (batch_size, 128, num_points, k) -> (batch_size, 128, num_points)
+
+        x = get_neighbors(x3, k=self.k)     # (batch_size, 128, num_points) -> (batch_size, 128*2, num_points, k)
+        x = self.conv4(x)                       # (batch_size, 128*2, num_points, k) -> (batch_size, 256, num_points, k)
+        x4 = x.max(dim=-1, keepdim=False)[0]    # (batch_size, 256, num_points, k) -> (batch_size, 256, num_points)
+
+        x = torch.cat((x1, x2, x3, x4), dim=1)  # (batch_size, 64+64+128+256, num_points)
+
+        x = self.conv5(x)                       # (batch_size, 64+64+128+256, num_points) -> (batch_size, emb_dims, num_points)
+        x1 = F.adaptive_max_pool1d(x, 1).view(batch_size, -1)           # (batch_size, emb_dims, num_points) -> (batch_size, emb_dims)
+        x2 = F.adaptive_avg_pool1d(x, 1).view(batch_size, -1)           # (batch_size, emb_dims, num_points) -> (batch_size, emb_dims)
+        x3 = torch.cat((x1, x2), 1)              # (batch_size, emb_dims*2)
+
+        x = F.leaky_relu(self.bn6(self.linear1(x3)), negative_slope=0.2) # (batch_size, emb_dims*2) -> (batch_size, 512)
+        x = self.dp1(x)
+        x = F.leaky_relu(self.bn7(self.linear2(x)), negative_slope=0.2) # (batch_size, 512) -> (batch_size, 256)
+        x = self.dp2(x)
+        x = self.linear3(x)                                             # (batch_size, 256) -> (batch_size, output_channels)
+        
+        y = self.drop1(F.relu(self.bn8(self.fc1(x3))))
+        y = self.drop2(F.relu(self.bn9(self.fc2(y))))
+        y = self.fc3(y)
+         
+        z = torch.cat((x3, x, y), dim=1)
+        
+        return x, y, z                       # x -> motor type classification.     y -> cover bolt classification
+        
+    
+if __name__ == '__main__':
+    from torchsummary import summary
+    # help(summary)
+    model = DGCNN_net()
+    summary(model, (3, 1024), device='cuda')
+    print(model)
+    
